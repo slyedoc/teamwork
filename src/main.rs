181a8fcfb33641ffa9372dd9ad2f523b6cb1e70b
Patrick Towles <james.towles@gmail.com>
2021-06-28 01:07:53 -0400

    add more iterators vs for loops

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ fn main() {
 
     let mut rng = rand::thread_rng();
     let length = rng.gen_range(10..15);
-    let letters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k' ];
-
-    // using iterators
-    let password_iter = (0..length)
-        .map(|_| {
-            let random_index = rng.gen_range(0..letters.len());
-            letters[random_index]
-        }).collect::<String>();
+    let mut letters = vec![];
+    
+    // Build list of letters to use, using for loop
+    for c in 'A'..'Z' {
+        letters.push(c);
+    }
+    // add to list using iterators
+    ('a'..'z').for_each(|c| letters.push(c) );
 
     // Same as above, but using a for loop instead
     let mut password_for = "".to_string();
@@ -20,7 +20,14 @@ fn main() {
         password_for.push(letters[random_index]);
     }
 
-    println!("iter = {:?}", password_iter);
-    println!("for = {:?}", password_for);
+    // using iterators
+    let password_iter = (0..length)
+    .map(|_| {
+        let random_index = rng.gen_range(0..letters.len());
+        letters[random_index]
+    }).collect::<String>();
+
+    println!("iter = {}", password_iter);
+    println!("for = {}", password_for);
 }
 
